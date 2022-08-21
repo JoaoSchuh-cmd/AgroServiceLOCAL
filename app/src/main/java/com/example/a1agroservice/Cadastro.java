c3cc14852adc4090128e3e78bb549899a5d2f45a
João Schuh <joao.schuh@edu.unipar.br>
2022-08-21 11:23:00 -0300

    Criando conexao http para comunicação com API

--- app/src/main/java/com/example/a1agroservice/Cadastro.java
+++ app/src/main/java/com/example/a1agroservice/Cadastro.java
@@ -3,8 +3,13 @@ package com.example.a1agroservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.a1agroservice.bufferedreader.Conexao;
 
 public class Cadastro extends AppCompatActivity {
 
@@ -17,5 +22,27 @@ public class Cadastro extends AppCompatActivity {
     public void btVoltarOnClick(View view) {
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
+    }
+
+    public void btSalvarOnCick(View view) {
+        Tarefa tarefa = new Tarefa();
+        tarefa.execute("http://10.0.2.2:27017/pessoa");
+    }
+
+    private class Tarefa extends AsyncTask<String, String, String> {
+
+        public Tarefa() {
+            super();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return Conexao.getDados(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Toast.makeText(Cadastro.this, s, Toast.LENGTH_SHORT).show();
+        }
     }
 }
