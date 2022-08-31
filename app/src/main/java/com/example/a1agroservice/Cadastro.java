10ce56e49e7000ab73b9c2107d56091e66e9a303
João Schuh <joao.schuh@edu.unipar.br>
2022-08-31 00:43:50 -0300

    Remoção AsyncTask

--- app/src/main/java/com/example/a1agroservice/Cadastro.java
+++ app/src/main/java/com/example/a1agroservice/Cadastro.java
@@ -25,24 +25,6 @@ public class Cadastro extends AppCompatActivity {
     }
 
     public void btSalvarOnCick(View view) {
-        Tarefa tarefa = new Tarefa();
-        tarefa.execute("http://10.0.2.2:27017/pessoa");
     }
 
-    private class Tarefa extends AsyncTask<String, String, String> {
-
-        public Tarefa() {
-            super();
-        }
-
-        @Override
-        protected String doInBackground(String... strings) {
-            return Conexao.getDados(strings[0]);
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            Toast.makeText(Cadastro.this, s, Toast.LENGTH_SHORT).show();
-        }
-    }
 }
