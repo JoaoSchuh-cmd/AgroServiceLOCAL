38496172ecc8b043ac64c5f55ed8669cc7100cde
João Schuh <joao.schuh@edu.unipar.br>
2022-11-15 11:56:13 -0300

    Implementação do WhatsApp

--- app/src/main/java/com/example/a1agroservice/fragments/AnunciosDetalhesFragment.java
+++ app/src/main/java/com/example/a1agroservice/fragments/AnunciosDetalhesFragment.java
@@ -2,11 +2,14 @@ package com.example.a1agroservice.fragments;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,12 +20,9 @@ import androidx.fragment.app.DialogFragment;
 import com.example.a1agroservice.R;
 import com.example.a1agroservice.controllers.AnuncioController;
 import com.example.a1agroservice.controllers.EnderecoController;
-import com.example.a1agroservice.controllers.PessoaController;
 import com.example.a1agroservice.controllers.ServicoController;
 import com.example.a1agroservice.controllers.TipoServicoController;
 import com.example.a1agroservice.models.Anuncio;
-
-import org.w3c.dom.Text;
 
 public class AnunciosDetalhesFragment extends DialogFragment {
     private Context context;
@@ -40,6 +40,8 @@ public class AnunciosDetalhesFragment extends DialogFragment {
     private TextView tvEstado;
     private TextView tvProprietario;
     private TextView tvValorHora;
+
+    private Button btEnviarMensagem;
 
     public AnunciosDetalhesFragment(Context context, Anuncio anuncio) {
         this.context = context;
@@ -73,6 +75,22 @@ public class AnunciosDetalhesFragment extends DialogFragment {
             tvEstado.setText(enderecoController.getEnderecoById(anuncio.getId_endereco()).getEstado());
             tvProprietario.setText(anuncio.getNomeProprietario());
             tvValorHora.setText(String.valueOf(servicoController.getServicoById(anuncio.getId_servico()).getValorhora()));
+
+            btEnviarMensagem = view.findViewById(R.id.btEnviarMensagem);
+
+            btEnviarMensagem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String phone = anuncio.getCelular();
+                    try {
+                        Intent whatsapp = new Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone="+phone));
+                        startActivity(whatsapp);
+                    } catch (Exception E) {
+                        Toast.makeText(context, "Erro ao tentar abrir WhatsApp", Toast.LENGTH_SHORT).show();
+                        Log.e("SendMessageClick", E.getMessage());
+                    }
+                }
+            });
 
         } catch (Exception E) {
             Toast.makeText(context, "Erro ao carregar tela de detalhes de anúncio", Toast.LENGTH_SHORT).show();
